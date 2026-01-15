140c43b0a2d63fc79f9122586163677d912f5116
lorenzogovernale47-star <lorenzogovernale47@gmail.com>
2026-01-15 13:23:00 +0000

    Prima versione

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,47 +2,14 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router";
 
 export const Root = () => {
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState("Titolo iniziale");
-
-  useEffect(() => {
-    if (count === 4) {
-      setTitle("Il titolo ha superato il 4");
-    }
-  }, [count]);
 
   return (
-    <div className="h-dvh flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-md shadow-lg">
-        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          {title}
-        </h1>
+    
+    <div class="flex flex-row">
+  <div class="basis-1/4 md:basis-1/3"><img src="https://media.istockphoto.com/id/517188688/it/foto/paesaggio-di-montagna.jpg?s=612x612&w=0&k=20&c=3-yra8c7GyAenb5DTfF1x5XKFqGB1Gp6vXAtTf5fDNM="></img></div>
+  <div class="basis-1/4 md:basis-1/3">02</div>
+  <div class="basis-1/2 md:basis-1/3">03</div>
+</div>
 
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
-
-        <div className="flex flex-col items-center space-y-4">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
-          </button>
-
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setTitle("Charizard")}
-          >
-            Cambia titolo
-          </button>
-
-          <Link
-            to="/frontend-rocks/dettaglio/1"
-            className="text-blue-500 underline"
-          >
-            Link alla pagina di dettaglio
-          </Link>
-        </div>
-      </div>
-    </div>
   );
 };
