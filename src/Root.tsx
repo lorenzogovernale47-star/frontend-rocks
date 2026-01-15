4a1c9df8e912ed0a9d510d86a29110fae0c73f39
lorenzogovernale47-star <lorenzogovernale47@gmail.com>
2026-01-15 15:18:37 +0000

    Quarta versione implementazione api

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from "react";
+import { PokeAPI } from "./api";
+
 export const Root = () => {
+const [pokemons, setPokemons] = useState<any[]>([]);
+
+  useEffect(() => {
+    PokeAPI.listPokemons().then((data) => {
+      setPokemons(
+        data.results.map((item, idx) => ({
+          id: idx,
+          name: item.name,
+          image: ""
+        }))
+      );
+    });
+  }, []);
 
   return (
-    
-    <div className="flex flex-row">
-  <div className="basis-1/4 md:basis-1/3"><img src="https://media.istockphoto.com/id/517188688/it/foto/paesaggio-di-montagna.jpg?s=612x612&w=0&k=20&c=3-yra8c7GyAenb5DTfF1x5XKFqGB1Gp6vXAtTf5fDNM="></img></div>
-</div>
-
+    <div>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.id}>{pokemon.name}</div>
+      ))}
+    </div>
   );
 };
